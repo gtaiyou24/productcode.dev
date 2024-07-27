fa79d442b5d21f34c95900619cd57fe2526d6aee
gtaiyou24 <gtaiyou24@gmail.com>
2024-07-27 09:41:10 +0900

    fix: logo.tsx

--- frontend/src/components/layout/navbar/logo.tsx
+++ frontend/src/components/layout/navbar/logo.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Logo({ width, height }: { width: number; height: number; }) {
     return (
         <>
-            <Image src='/logo-dark.png' className={"hidden light:block"} alt={APP_NAME} width={width} height={height} />
+            <Image src='/logo-dark.png' className={"hidden dark:block"} alt={APP_NAME} width={width} height={height} />
             <Image src='/logo-light.png' className={"block dark:hidden"} alt={APP_NAME} width={width} height={height} />
         </>
     );
