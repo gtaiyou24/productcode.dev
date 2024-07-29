3dbaca20fb44425425d45fef3ff0b2c567f52c77
gtaiyou24 <gtaiyou24@gmail.com>
2024-07-30 00:09:59 +0900

    feat: filter枠を追加

--- frontend/src/app/(service)/page.tsx
+++ frontend/src/app/(service)/page.tsx
@@ -5,10 +5,15 @@ import ProductGridItems from "@/components/layout/product-grid-items";
 export default function Home() {
   const products = PRODUCTS;
   return (
-    <div className="p-6 mx-auto max-w-screen-xl">
-      <Grid className="grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-        <ProductGridItems products={products} />
-      </Grid>
+    <div className="p-6 mx-auto flex max-w-screen-xl flex-col gap-8 px-4 pb-4 text-black md:flex-row dark:text-white">
+      <div className="order-first w-full flex-none md:max-w-[200px]">
+          Filter
+      </div>
+      <div className="order-last min-h-screen w-full md:order-none">
+        <Grid className="grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+          <ProductGridItems products={products} />
+        </Grid>
+      </div>
     </div>
   );
 }
