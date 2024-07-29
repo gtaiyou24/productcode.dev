b815c2f92379e0ba815c546ac9e83c0d766eb7bc
gtaiyou24 <gtaiyou24@gmail.com>
2024-07-30 00:00:11 +0900

    style: gap-4 -> gap-2

--- frontend/src/components/grid/index.tsx
+++ frontend/src/components/grid/index.tsx
@@ -2,7 +2,7 @@ import clsx from 'clsx';
 
 function Grid(props: React.ComponentProps<'ul'>) {
   return (
-    <ul {...props} className={clsx('grid grid-flow-row gap-4 lg:gap-4', props.className)}>
+    <ul {...props} className={clsx('grid grid-flow-row gap-2 lg:gap-4', props.className)}>
       {props.children}
     </ul>
   );
