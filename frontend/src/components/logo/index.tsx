5c97a27cfee359f651b02a120e70e7f481cc76a1
gtaiyou24 <gtaiyou24@gmail.com>
2024-07-30 00:19:27 +0900

    style: add rounded to logo

--- frontend/src/components/logo/index.tsx
+++ frontend/src/components/logo/index.tsx
@@ -2,11 +2,17 @@ import {APP_NAME} from "@/lib/constants";
 import Image from "next/image";
 
 
-export default function Logo({ width, height }: { width: number; height: number; }) {
+export default function Logo({
+    width,
+    height
+}: {
+    width: number;
+    height: number;
+}) {
     return (
         <>
-            <Image src='/logo-dark.png' className={"hidden dark:block"} alt={APP_NAME} width={width} height={height} />
-            <Image src='/logo-light.png' className={"block dark:hidden"} alt={APP_NAME} width={width} height={height} />
+            <Image src='/logo-dark.png' className={"rounded-lg hidden dark:block"} alt={APP_NAME} width={width} height={height} />
+            <Image src='/logo-light.png' className={"rounded-lg block dark:hidden"} alt={APP_NAME} width={width} height={height} />
         </>
     );
 }
