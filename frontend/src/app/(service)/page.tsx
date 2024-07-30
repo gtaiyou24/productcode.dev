cb1b50218559a7da35e425262da76435edd430e8
gtaiyou24 <gtaiyou24@gmail.com>
2024-07-30 13:50:18 +0900

    install components/ui/input.tsx

--- frontend/src/app/(service)/page.tsx
+++ frontend/src/app/(service)/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   const products = PRODUCTS;
   return (
     <div className="p-6 mx-auto flex max-w-screen-xl flex-col gap-8 px-4 pb-4 text-black md:flex-row dark:text-white">
-      <div className="order-first w-full flex-none md:max-w-[200px]">
+      <div className="order-first w-full flex-none md:max-w-[250px]">
           <FilterList list={filterList} />
       </div>
       <div className="order-last min-h-screen w-full md:order-none">
--- frontend/src/components/layout/search/filter/index.tsx
+++ frontend/src/components/layout/search/filter/index.tsx
@@ -3,16 +3,17 @@ import {Search} from "lucide-react";
 import {FilterListItem} from "@/lib/types";
 import {FilterItem} from "@/components/layout/search/filter/item";
 import FilterItemDrawer from "@/components/layout/search/filter/drawer";
+import {Input} from "@/components/ui/input";
 
 const FilterItemList = ({ list }: { list: FilterListItem[]; }) => {
     return (
         <>
-            <div className="relative mb-4">
+            <div className="relative text-sm mb-4">
                 <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" size={18} />
-                <input
+                <Input
                     type="text"
                     placeholder="Search..."
-                    className="w-full bg-gray-50 text-black dark:bg-gray-800 dark:text-white pl-10 pr-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-current"
+                    className="bg-gray-50 text-black dark:bg-gray-800 dark:text-white pl-10 pr-4 py-2 rounded-md"
                 />
             </div>
             <ul>
